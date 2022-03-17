ee7219e8be8b01479e9cc7d5f7f32b98ab249994
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-03-16 22:08:24 -0500

    modificando componente raiz creando propiedades, primero carga el main, luego app.module, luego app.componet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'MI primera aplicacion';
+
+  saludo = 'Hola alumnos de angular';
 }
