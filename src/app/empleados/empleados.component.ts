0bb9b26028848ae80189b2f7d6f8c62871a2e880
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-03-16 22:28:47 -0500

    creando componentes

---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+//DECORADOR
+@Component({
+    selector:"app-empleados",//nombre de la etiqueta
+    templateUrl:"./empleados.component.html",
+    styleUrls:["./empleados.component.css"],
+})
+//CLASE
+export class EmpleadosComponent {
+
+}
