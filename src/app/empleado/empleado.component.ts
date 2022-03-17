ee2ea5bccbc5061943817588c31f74191a483929
saul <saul@.hotmail.com>
2022-03-17 10:28:56 -0500

    interpolacion texto dinamico, creando propiedades

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent implements OnInit {
+//INTEPOLACION = TEXTO DINAMICO
 
+nombre = "JUAN";
+apellido = "PEREZ";
+private edad = 8;
+empresa="CANVIA";
+
+getEdad(){
+  return this.edad;
+}
   constructor() { }
 
   ngOnInit(): void {
