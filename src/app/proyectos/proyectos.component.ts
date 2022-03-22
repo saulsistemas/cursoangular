e1e8c6523edd88859ca2602133fe4cf393f415bd
saul <saul@.hotmail.com>
2022-03-22 11:39:31 -0500

    rutas en botones, redireccionamiento automaticos

--- src/app/proyectos/proyectos.component.ts
+++ src/app/proyectos/proyectos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../form-empleado/empleado.model';
+import { EmpleadoService } from '../servicios/empleados.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyectos.component.css']
 })
 export class ProyectosComponent implements OnInit {
-
-  constructor() { }
+  empleados:Empleado[]=[];
+  constructor(private router:Router,private empleadosService:EmpleadoService) { }
 
   ngOnInit(): void {
+    this.empleados=this.empleadosService.empleados;
   }
+  volveHome(){
+    this.router.navigate(['']);
+  }
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
 
+  agregarEmpleado(){
+    let miEmpelado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    //this.miServicio.muestraMensaje("nombre del empleado " + miEmpelado.nombre)
+    this.empleadosService.agregarEmpleadoServicio(miEmpelado);
+    console.log(this.empleados);
+    this.volveHome();
+  }
 }
